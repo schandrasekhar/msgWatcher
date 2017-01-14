4539b8503c9b2f7705d49cb0961a22204a44a35c
srigiriraju <srigiriraju@yahoo-inc.com>
2017-01-14 09:31:50 -0800

    fix: more code

--- src/modules/app/watcher.py
+++ src/modules/app/watcher.py
@@ -5,16 +5,16 @@ import pyinotify
 
 wm = pyinotify.WatchManager()
 # Associate this WatchManager with a Notifier (will be used to report and
-# process events).
-notifier = pyinotify.Notifier(wm)
-# Add a new watch on /tmp for ALL_EVENTS.
-wm.add_watch('/tmp', pyinotify.ALL_EVENTS)
-# Loop forever and handle events.
-def callback(obj):
-    print(obj)
+# # process events).
+# notifier = pyinotify.Notifier(wm)
+# # Add a new watch on /tmp for ALL_EVENTS.
+# wm.add_watch('/tmp', pyinotify.ALL_EVENTS)
+# # Loop forever and handle events.
+# def callback(obj):
+#     print(obj)
 
 
-notifier.loop(callback=callback)
+# notifier.loop(callback=callback)
 
 
 # steps for this feature
@@ -27,12 +27,24 @@ notifier.loop(callback=callback)
 # the events for which i will want to do some functionality, in terms of (js) execute a
 # callback
 
-# class Watcher(object):
-#     """docstring for Watcher"""
-#     def __init__(self):
-#         super(Watcher, self).__init__()
-#     def getWatcher():
-#         # Instanciate a new WatchManager (will be used to store watches).
-#         wm = pyinotify.WatchManager()
-#         return wm
-    
+class Watcher(object):
+    """docstring for Watcher"""
+    def __init__(self, path, event):
+        super(Watcher, self).__init__()
+        self._path = path
+        self._event = event
+    def _bind(self):
+        self._wm.add_watch(self._path, self._event)
+    def getNotifier(self):
+        wm = pyinotify.WatchManager()
+        self._wm = wm
+        self._notifier = pyinotify.Notifier(self._wm)
+        self._bind(self)
+        return self._notifier
+
+path = "/tmp"
+event = IN_MODIFY
+watcher = Watcher(path, event)
+notifier = watcher.getNotifier()
+notifier.loop(callback=callback)
+
